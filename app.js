d3cb3e6c1242bb243c641a85320f9eed943b4079
kevin_wang@sjtu.edu.cn <kevin_wang@sjtu.edu.cn>
2020-08-18 16:03:47 +0800

    添加权限管理

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // 初始化配置文件
-const config = require("./utils/config");
+const config = require("./utils/config").readConfigSync();
+const authority = require("./config/authority");
+
+const axios = require("axios");
 
 // 初始化Express服务器
 const express= require("express");
@@ -13,8 +16,53 @@ app.use(bodyparser.json());
 // 允许跨域请求
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Cache-Control","no-cache");
-    res.header("Access-Control-Allow-Methods","PUT,PATCH,POST,GET,DELETE,OPTIONS");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Access-Control-Allow-Methods", "PUT,PATCH,POST,GET,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    if(req.path === "/favicon.ico"){
+        res.replSetGetStatus(404);
+    } else {
+        next();
+    }
+});
+
+// 权限管理
+app.use(async (req, res, next) => {
+    let arrPath = req.path.split("/");
+    if(arrPath[0] === '') arrPath.shift();
+    let cpyAuth = authority;
+    for(let i = 0; i < arrPath.length; i++) {
+        if(cpyAuth.hasOwnProperty(arrPath[i]))
+            cpyAuth = cpyAuth[arrPath[i]];
+        else break;
+    }
+    if(cpyAuth.hasOwnProperty(req.method.toLowerCase())) {
+        let authReq = cpyAuth[req.method.toLowerCase()];
+        if(authReq > -1) {
+            if(req.query.token) {
+                req.headers.authorization = req.headers.token;
+            }
+            req.headers.authorization = req.headers.authorization || "";
+            let ip =  req.headers['x-forwarded-for'] ||
+                req.connection.remoteAddress ||
+                req.socket.remoteAddress ||
+                req.connection.socket.remoteAddress;
+            let result = await axios({
+                method: "GET",
+                headers: {authorization: req.headers.authorization},
+                url: "http://" + config.auth.ip + ":" + config.auth.port.toString() + "/auth?json=true&authority=" + authReq.toString() + "&origin=" + ip
+            });
+            if(!result.data.token)  {
+                res.sendStatus(401);
+                res.end(JSON.stringify({error: "Not enough authority"}));
+                return;
+            }
+        }
+    }
+    next();
+});
+
+app.use((req, res, next) => {
     if (req.method.toLowerCase() === 'options') {
         res.sendStatus(200);  // 让options尝试请求快速结束
     } else {
@@ -35,4 +83,4 @@ app.use((req, res) => {
 });
 
 // 监听
-app.listen(config.readConfigSync().port);
+app.listen(config.port);
--- readme.md
+++ readme.md
@@ -8,11 +8,15 @@
 
     参数: 无
 
+    权限需求: 管理员(200)
+
     返回: 区域数组，区域格式参考数据建模表
 
   - GET /map/zone/id 列出指定ID的区域
 
     参数: URL参数 id 必须为有效ObjectID
+
+    权限需求: 管理员(200)
 
     返回: 若参数正确，则返回区域数据；若找不到区域，则返回404，{"error":"No corresponding result"}
 
@@ -20,17 +24,23 @@
 
     参数: 请求体参数 type: ban/restriction/garage, vertex: 二维坐标数组; 若type为restriction要附加min-height参数表面最低高度 
 
+    权限需求: 管理员(500)
+
     返回: 若参数正确，则返回{n: 1, ok: 1}
 
   - PATCH /map/zone/id 修改区域信息
 
     参数: URL参数 id 必须为有效ObjectID; 请求体参数 type: ban/restriction/garage; 若type为restriction要附加min-height参数表面最低高度 
 
+    权限需求: 管理员(500)
+
     返回: 若参数正确，则返回{n: 1, nModified: x, ok: 1}若x为0则无修改内容，x为1则有修改内容
 
   - DELETE /map/zone/id 删除区域
 
     参数: URL参数 id 必须为有效ObjectID
+
+    权限需求: 管理员(500)
 
     返回: 若参数正确，则返回{n: 1,ok: 1}
 
@@ -40,6 +50,8 @@
 
     参数: URL参数 id 必须为有效ObjectID
 
+    权限需求: 管理员(200)
+
     返回: 若参数正确，则返回路径数据；若找不到路径，则返回404，{"error":"No corresponding result"}
 
     注: 由于区域为异步运算，status为pending则表明运算未完成，为OK则表明运算完成，由于区域数据量大，不提供批量查找
@@ -47,6 +59,8 @@
   - POST /map/routine 新建路径
 
     参数: querystring参数 multi 若为true则进入多点模式，为false则进入单点模式; 多点模式下 请求体参数 POI为所有途径点的二或三维坐标数组（有序）; 单点模式下 请求体参数 from及to为起点与终点两个二或三维数组
+
+    权限需求: 管理员(500)
 
     返回: {id: ObjectID}为创建路径的ID
 
@@ -58,11 +72,15 @@
 
     参数: 无
 
+    权限需求: 管理员(200)
+
     返回: 地图钉数组
 
   - GET /map/pinpoint/id 列出指定ID的地图钉
 
     参数: URL参数 id 必须为有效ObjectID
+
+    权限需求: 管理员(200)
 
     返回: 若参数正确，则返回地图钉数据；若找不到地图钉，则返回404，{"error":"No corresponding result"}
 
@@ -70,17 +88,23 @@
 
     参数: 请求体参数 type: point/terminal, coordinate: 三维坐标 
 
+    权限需求: 管理员(500)
+
     返回: 若参数正确，则返回{n: 1, ok: 1}
 
   - PATCH /map/pinpoint/id 修改地图钉信息
 
     参数: URL参数 id 必须为有效ObjectID; 请求体参数 type: point/terminal
 
+    权限需求: 管理员(500)
+
     返回: 若参数正确，则返回{n: 1, nModified: x, ok: 1}若x为0则无修改内容，x为1则有修改内容
 
   - DELETE /map/pinpoint/id 删除地图钉
 
     参数: URL参数 id 必须为有效ObjectID
+
+    权限需求: 管理员(500)
 
     返回: 若参数正确，则返回{n: 1, ok: 1}
 
@@ -90,16 +114,22 @@
 
     参数: URL参数 pin 必须为有效ObjectID
 
+    权限需求: 管理员(200)
+
     返回: 若参数正确，则返回关键点信息
 
   - GET /map/path/pin1/pin2 列出两点间的关键路径
 
     参数: URL参数 pin1, pin2 必须为有效ObjectID
 
+    权限需求: 管理员(200)
+
     返回: 若参数正确，则返回关键点信息；若找不到路径，则返回404，{"error":"No corresponding result"}
 
   - POST /map/path 建立关键路径
 
     参数: 请求体参数 routine为寻路ID, from与to为地图钉ID
+
+    权限需求: 管理员(500)
 
     返回: 若参数正确，则返回{n: 1, ok: 1}
